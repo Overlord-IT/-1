feb9cc2471dd42ca9986f0a76c57bfe6ed21c4bb
Daniar <daniar20014@gmail.com>
2024-10-22 00:17:12 +0700

    Update main_1.py

--- Лабораторная 1/main_1.py
+++ Лабораторная 1/main_1.py
@@ -1,17 +1,15 @@
 numbers = [2, -93, -2, 8, None, -44, -1, -85, -14, 90, -22, -90, -100, -8, 38, -92, -45, 67, 53, 25]
 
 # TODO заменить значение пропущенного элемента средним арифметическим
-
-# Количество
-count_of_numbers = len(numbers)
+skip_fives1 = numbers[:4]
 # Сумма
 skip_fives = numbers[5:]
-sum_of_numbers = sum(skip_fives)
+sum_of_numbers = sum(skip_fives1) + sum(skip_fives)
+# Количество
+count_of_numbers = len(numbers)
 # Среднее арифметическое
-average_of_numbers = round(sum_of_numbers / count_of_numbers)
+average_of_numbers = (sum_of_numbers / count_of_numbers)
 # Замена цифры
 numbers[4] = average_of_numbers
 
 print("Измененный список:", numbers)
-
-# Я думаю у вас ошибка в ответе
